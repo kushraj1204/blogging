9fa6f36e764209cf1ee73a3ef56c14d9c6d76880
Kush <kushraj1204@gmail.com>
2021-07-02 11:19:05 +0545

    when debug is true various errors arise except for that all okay

--- rental/settings.py
+++ rental/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gte45$0cn@mrd&9m&w(vnxvhgo!tj*q&08qcc-&%q%n1z)j%z6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = "Users.CustomUser"
